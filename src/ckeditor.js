862d6cc56c89def6f3d1ee9114ceb80f3a94765a
CC\BRUNETE <brunete@ccf.org>
2023-02-16 14:08:43 -0500

    Added project files

---
+++ src/ckeditor.js
@@ -0,0 +1,87 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+  Alignment,
+	AutoLink,
+	Autosave,
+	Bold,
+	Code,
+	DataFilter,
+	Essentials,
+	FindAndReplace,
+	Heading,
+	Indent,
+	Italic,
+	Link,
+	List,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	SourceEditing,
+	Subscript,
+	Superscript,
+	Underline
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'undo',
+			'redo',
+			'|',
+      'heading',
+      'alignment',
+			'bold',
+			'italic',
+      'underline',
+			'removeFormat',
+			'|',
+			'subscript',
+			'superscript',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'outdent',
+			'indent',
+			'|',
+			'link',
+			'|',
+			'sourceEditing',
+			'|',
+			'findAndReplace'
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
